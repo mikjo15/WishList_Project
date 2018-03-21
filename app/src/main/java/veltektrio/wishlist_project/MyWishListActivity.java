3170dc93c535b18cb7eb0ceee9705d90547d6110
Cathrine <cathrine.stokkebaek@gmail.com>
2018-03-21 17:42:25 +0100

    Merge branch 'master' of C:\Users\CathrineBuch\AndroidStudioProjects\WishList_Project with conflicts.

--- app/src/main/java/veltektrio/wishlist_project/MyWishListActivity.java
+++ app/src/main/java/veltektrio/wishlist_project/MyWishListActivity.java
@@ -1,15 +1,20 @@
 package veltektrio.wishlist_project;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MyWishListActivity extends AppCompatActivity {
 
     public static Wishlist myWishlist;
+    private Fragment fragment_popup;
+    private FragmentManager mfragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,20 @@ public class MyWishListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mfragmentManager = getSupportFragmentManager();
+        fragment_popup = new AddWishFragment();
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               mfragmentManager.beginTransaction().replace(R.id.?????????, fragment_popup).commit();
+               //overskriv det fragment der viser cardview
+                //Det samme skal gøres inden fra fragment ved add wish button. Her kan man tilgå aktivitenten og derfor altid fragment
             }
         });
+
     }
 
 }
--- app/src/main/java/veltektrio/wishlist_project/Wishlist.java
+++ app/src/main/java/veltektrio/wishlist_project/Wishlist.java
@@ -8,7 +8,7 @@ import java.util.Vector;
 
 public class Wishlist extends Wish {
 
-    public Wishlist(){}
+    public Wishlist(String user_name){ this.user_name = user_name; }
 
     public void remove_wish(Wish w){
         wishlist.remove(w);
@@ -16,8 +16,21 @@ public class Wishlist extends Wish {
     public void add_wish(Wish w){
         wishlist.addElement(w);
     }
+    public int getSize() { return wishlist.size(); }
+    public Wish getPosition(int position) {
+        return wishlist.get(position);
+    }
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
 
     private Vector<Wish> wishlist;
+
+    private String user_name;
 }
 
 
